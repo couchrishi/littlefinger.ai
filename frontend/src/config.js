afef488bee685974743d4149fa904013a0f1d896
Sai Balaji Rajaraman <saibalaji@google.com>
2024-12-23 14:04:55 +0800

    backend changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,37 +1,10 @@
 const isLocal = process.env.NODE_ENV === "development";
 
-// const config = {
-//   API_BASE_URL: isLocal 
-//     ? "http://127.0.0.1:5001/saib-ai-playground/us-central1" // Local emulator
-//     : "https://littlefinger.ai", // Production URL
-// };
-
-//Cloud Function URL - https://us-central1-saib-ai-playground.cloudfunctions.net/chatWithAI
-
-// export default config;
-
-// const config = {
-//   API_BASE_URL: isLocal
-//     ? "http://127.0.0.1:5001/saib-ai-playground/us-central1"
-//     : "https://us-central1-saib-ai-playground.cloudfunctions.net",
-// };
-// export default config;
-
-
 const config = {
   API_BASE_URL: isLocal
     ? "http://127.0.0.1:3001/api/sendmessage"
-    : "https://us-central1-saib-ai-playground.cloudfunctions.net",
+    : "https://littlefinger-backend-isolated-288406675721.us-central1.run.app/api/sendmessage",
+    
 };
 export default config;
-
-
-
-
-
-// const config = {
-//   API_BASE_URL: isLocal
-//     ? "https://chatwithai-ec7vmxv2ma-uc.a.run.app"
-//     : "https://chatwithai-ec7vmxv2ma-uc.a.run.app",
-// };
 
